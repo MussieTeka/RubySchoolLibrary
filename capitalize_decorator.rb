6bfb76a621df28568fe27ac23a7745130b4d1509
Mussie Kahsay <mussiekahasay12@gmail.com>
2023-06-24 19:15:21 +0300

    Modify capitalize_decorator.rb file.

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,7 +1,7 @@
 require_relative 'base_decorator'
 
 class CapitalizeDecorator < BaseDecorator
-  def get_correct_name
-    @nameable_obj.get_correct_name.capitalize
+  def correct_name
+    @nameable_obj.correct_name.capitalize
   end
 end
